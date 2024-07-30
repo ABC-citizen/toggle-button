2bef3788c9e1a22775691caccd496227e4dd8f59
ABC-citizen <westernenglishcartoon@gmail.com>
2024-07-30 06:41:29 -0700

    Toggle button by using html,css and js

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ var button =document.getElementById("btn");
 button.onclick=function(){
     document.body.classList.toggle('dark-theme');
     
+    if(button.innerHTML=="Dark Mode"){
+        button.innerHTML='Light Mode';
+    }else{
+        button.innerHTML='Dark Mode';
+    }
 }
-
